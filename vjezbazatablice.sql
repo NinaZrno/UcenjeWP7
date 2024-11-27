043c6dabeae4226a3dda8d80829ff62442de0603
NinaZrno <ninaradakovic1234@icloud.com>
2024-11-27 21:00:21 +0100

    Create vjezbazatablice.sql

---
+++ vjezbazatablice.sql
@@ -0,0 +1,24 @@
+﻿use master;
+go
+drop vjezbezatablice;
+go
+create database vjezbezatablice;
+go
+
+use vjezbezatablice;
+
+create table ispitnirokovi(
+sifra int,
+predmet varchar(50),
+vrstaispita int,
+datum datetime,
+pristupio varchar(50)
+);
+
+
+create table pristupnici(
+ispitnirok int,
+student varchar(50),
+brojbodova int,
+ocjena int
+);
