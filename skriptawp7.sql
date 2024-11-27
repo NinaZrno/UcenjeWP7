3bb5eb5bb0fcf6cf29e5fe48a96d46d7b33b3e31
NinaZrno <ninaradakovic1234@icloud.com>
2024-11-27 19:15:20 +0100

    osnovne naredbe na drop ako postoji i kreiranje baze

--- skriptawp7.sql
+++ skriptawp7.sql
@@ -1,6 +1,12 @@
-﻿--create database edunovawp7;
+﻿
 
---use edunovawp7;
+use master;
+go
+drop database if exists edunovawp7;
+go
+create database edunovawp7;
+go
+use edunovawp7;
 
 create table smjer(
 naziv varchar (50)
