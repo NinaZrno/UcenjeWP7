c7e8dbeb74408863b4c6558e2e22ba48d73d87d9
NinaZrno <ninaradakovic1234@icloud.com>
2024-12-09 20:22:23 +0100

    Update zavrsnirad.sql

--- zavrsnirad.sql
+++ zavrsnirad.sql
@@ -13,14 +13,14 @@ sifra int not null primary key identity(1,1),
 naziv varchar(50) not null,
 vrsta varchar(60) not null,
 uputa text not null,
-trajanje varchar(50) 
+trajanje int 
 
 );
 
 
 create table sastojci(
 sifra int not null primary key identity(1,1),
-naziv int not null references recepti(sifra),
+naziv varchar(50) not null,
 podrijetlo varchar(50) not null,
 energija decimal(18,2) not null,
 ugljikohidrati decimal(18,2) not null,
@@ -35,10 +35,10 @@ sol decimal(18,2) not null
 
 create table sastavi(
 sifra int not null primary key identity(1,1),
-recept int not null references recepti(sifra),
-sastojak varchar(1000) not null, 
+recept varchar(500) not null,
+sastojak int not null references sastojci(sifra),
 kolicina decimal(18,2) not null,
-napomena varchar(1000) not null
+napomena varchar(1000) 
 
 );
 
@@ -50,16 +50,67 @@ da se ulje ne pregrije. 3.Korak Sa dvije zlice manipulirati tijestom tako da dob
 okruglice. 4.Korak Kuglice ubaciti u zagrijano ulje, kada kuglice porumene treba ih 
 okrenuti na drugu stranu i cekati da se do kraja isprze. 5.Korak Kuglice premjestiti
 u posudu oblozenu papirnatim rucnicima i zatim sve posipati secerom u prahu. 6.Korak 
-Uzivajte u svojim fritulama.', '30 minuta');
+Uzivajte u svojim fritulama.', 30);
 
 --1
 insert into sastojci( naziv, podrijetlo, energija, ugljikohidrati,
 masti, zasiceni_seceri, vlakna, bjelancevine, sol) values 
-( 1, 'Hrvatska', 1575.00, 24.90, 30.60, 80.00,220.00, 25.00, 7.00); 
+( 'Jogurt', 'Rusija', 60, 4.50, 2.80, 4.50, 0.17, 3.70, 0.13);
+
+--2
+insert into sastojci( naziv, podrijetlo, energija, ugljikohidrati,
+masti, zasiceni_seceri, vlakna, bjelancevine, sol) values 
+('Jaje', 'EU', 167, 1.50, 11.00, 2.50, 0.00, 13.00, 0.12);
+
+--3
+insert into sastojci( naziv, podrijetlo, energija, ugljikohidrati,
+masti, zasiceni_seceri, vlakna, bjelancevine, sol) values 
+('Secer', 'Polinezija', 387, 0.00, 0.00, 100.00, 0.00, 0.00, 0.00)
+
+--4
+insert into sastojci( naziv, podrijetlo, energija, ugljikohidrati,
+masti, zasiceni_seceri, vlakna, bjelancevine, sol) values 
+('Vanilin secer', 'Madagaskar', 387, 0.00, 0.00, 99.00, 0.00, 0.00, 1.00);
+
+--5
+insert into sastojci( naziv, podrijetlo, energija, ugljikohidrati,
+masti, zasiceni_seceri, vlakna, bjelancevine, sol) values 
+('Sol','Kina',0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 100.00);
+
+--6
+insert into sastojci( naziv, podrijetlo, energija, ugljikohidrati,
+masti, zasiceni_seceri, vlakna, bjelancevine, sol) values 
+('Prasak za pecivo', 'Engleska', 4.70, 0.00, 0.00, 0.00, 0.00, 1.25, 0.28);
+
+--7
+insert into sastojci( naziv, podrijetlo, energija, ugljikohidrati,
+masti, zasiceni_seceri, vlakna, bjelancevine, sol) values 
+('Rakija','Srbija', 185.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00);
+
+--8
+insert into sastojci( naziv, podrijetlo, energija, ugljikohidrati,
+masti, zasiceni_seceri, vlakna, bjelancevine, sol) values
+('Brasno','Srednji Istok', 364, 73.00, 1.70, 2.20, 2.70, 12.00, 0.12);
+
+
 
 --1
 insert into sastavi(sifra,recept,sastojak, kolicina, napomena) values
-(1,1,'200ml tekuceg jogurta, 1 jaje, 70g secera, 1 vrecica
-vanilin secera, prstohvat soli, 1/2 vrecice praska za pecivo,
-1 mala zlicica rakije, 200 g glatkog brasna, secer u prahu', 400.00,
-'Posluziti uz razne vrste dzemova.');
+(1,'Fritule',1, 200.00, 'Jogurt drzati izvan hladnjaka 30 minuta prije koristenja');
+
+--2
+insert into sastavi(sifra,recept,sastojak, kolicina, napomena) values
+(1,'Fritule',2,1.00, 'Koristiti jaje srednje velicine');
+
+--3
+insert into sastavi(sifra,recept,sastojak, kolicina, napomena) values
+(1,'Fritule',3,7, 'Najbolje koristiti morsku sol.');
+
+
+--4
+insert into sastavi(sifra,recept,sastojak, kolicina) values
+(1,'Fritule',4,1);
+
+--5
+insert into sastavi(sifra,recept,sastojak, kolicina) values
+(1,'Fritule',
