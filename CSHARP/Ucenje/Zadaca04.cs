8d22a372a883626e21b42e0b078070b8b42af471
Nina Zrno <ninaradakovic1234@icloud.com>
2025-01-11 22:34:17 +0100

    Create Zadaca04.cs

---
+++ CSHARP/Ucenje/Zadaca04.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ucenje {
+
+    internal class zadaca04 
+{
+    
+    public static void Izvedi()
+        {
+
+            Console.Write("Unesi broj ocjena: ");
+            int a= int.Parse(Console.ReadLine());
+
+
+
+
+
+
+        }
+    
+    
+    
+    }
+
+}
