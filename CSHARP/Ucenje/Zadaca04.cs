7c6063a0d40e2bb8fa0718bf6ebc73f19d1161c3
Nina Zrno <ninaradakovic1234@icloud.com>
2025-01-12 20:51:43 +0100

    Update Zadaca04.cs

--- CSHARP/Ucenje/Zadaca04.cs
+++ CSHARP/Ucenje/Zadaca04.cs
@@ -13,8 +13,20 @@ namespace Ucenje {
         {
 
             Console.Write("Unesi broj ocjena: ");
-            int a= int.Parse(Console.ReadLine());
+            
+                int a = int.Parse(Console.ReadLine());
 
+                int[] ocjene = new int[a];
+            
+
+            Console.WriteLine("Unesi ocjene: ");
+            {
+                int i=0 i<a i++
+                    Console.Write("Ocjena {0}: ", i + 1);
+
+            }
+            int zbroj = 0; 
+            int i =0 , i< a , i++
 
 
 
